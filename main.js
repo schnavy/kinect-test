a4ef57dbe3f75ecb908a297d06044f0f1f00ae59
David <50714711+schnavy@users.noreply.github.com>
2021-04-28 14:02:57 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,27 @@
+let kinectron = new Kinectron("172.20.10.4");
+kinectron.setKinectType("windows");
+kinectron.makeConnection();
+
+kinectron.startTrackedBodies(action);
+
+
+const circle = document.querySelector("#circle")
+
+
+
+
+
+
+function action(body) {
+    console.log(body.joints[0].cameraZ);
+    let depth = body.joints[0].cameraZ;
+    let X = body.joints[0].cameraX
+    circle.style.top = Math.floor(map(depth, 0, 5, 0, window.innerHeight)) + "px"
+    circle.style.left = Math.floor(map(X, -1, 1, 0, window.innerWidth)) + "px"
+    console.log(body.joints[0].cameraX);
+}
+
+
+function map(value, x1, y1, x2, y2) {
+    return ((value - x1) * (y2 - x2)) / (y1 - x1) + x2;
+}
